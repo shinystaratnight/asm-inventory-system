b2ceb3a793a2e043f553ea3a41a31aa2b2fa2802
Chris K <star-atnight@outlook.com>
2021-03-22 16:28:20 +0800

    change settings

--- core/settings.bk.py
+++ core/settings.bk.py
@@ -137,10 +137,10 @@ LOGIN_URL = '/login'
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, 'static')
-#]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static')
+]
 
 
 # Django Core Send Email
