2944f6d1eaa0f94ab7241e8d6c7828f56d8d8e99
Chris K <star-atnight@outlook.com>
2021-03-25 04:46:28 +0800

    fix up

--- static/js/customs/datatables_basic.js
+++ static/js/customs/datatables_basic.js
@@ -40,34 +40,6 @@ document.addEventListener('DOMContentLoaded', function() {
     $('.datatable-basic').DataTable();
 
 
-    // Alternative pagination
-    $('.datatable-pagination').DataTable({
-        pagingType: "simple",
-        language: {
-            paginate: {'next': $('html').attr('dir') == 'rtl' ? 'Next &larr;' : 'Next &rarr;', 'previous': $('html').attr('dir') == 'rtl' ? '&rarr; Prev' : '&larr; Prev'}
-        }
-    });
-
-
-    // Datatable with saving state
-    $('.datatable-save-state').DataTable({
-        stateSave: true
-    });
-
-
-    // Scrollable datatable
-    var table = $('.datatable-scroll-y').DataTable({
-        autoWidth: true,
-        scrollY: 300
-    });
-
-    // Resize scrollable table when sidebar width changes
-    $('.sidebar-control').on('click', function() {
-        table.columns.adjust().draw();
-    });
-
-
-
     // External table additions
     // ------------------------------
 
