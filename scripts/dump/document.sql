506c16c1b31932a287f3ca845f718bb7cf5498f3
Chris K <star-atnight@outlook.com>
2021-03-26 17:08:06 +0800

    adding sql files

--- scripts/dump/document.sql
+++ scripts/dump/document.sql
@@ -1,4 +1,4 @@
-INSERT INTO `masterdata_other` (`id`, `name`, `term`, `taxation`) VALUES
+INSERT INTO `masterdata_document` (`id`, `name`, `term`, `taxation`) VALUES
 (1, '出庫手数料', '売上高', '課税売上10%'),
 (2, '元払発送料', '売上高', '課税売上10%'),
 (3, '出庫手数料', '仕入高', '課対仕入10%'),
