8a5697a114cb40e20e5a475dba39e9f408c80019
Chris K <star-atnight@outlook.com>
2021-03-27 11:43:27 +0800

    fix up

--- contracts/views.py
+++ contracts/views.py
@@ -27,16 +27,21 @@ class TraderSalesContractView(AdminLoginRequiredMixin, TemplateView):
         context = super().get_context_data(**kwargs)
         context['contract_id'] = generate_contract_id()
         context['documents'] = Document.objects.all().values('name')
-        context['productformset'] = ProductFormSet()
+        context['productformset'] = ProductFormSet(prefix='product')
+        context['documentformset'] = DocumentFormSet(prefix='document')
         return context
 
 
-class TraderSalesValidatorView(AdminLoginRequiredMixin, View):
+class TraderSalesValidateView(AdminLoginRequiredMixin, View):
     def post(self, *args, **kwargs):
         if self.request.method == 'POST' and self.request.is_ajax():
-            print("Ajax")
+            product_formset = ProductFormSet(self.request.POST, prefix='product')
+            if not product_formset.is_valid():
+                return JsonResponse({'success': False}, status=400)
+            # document_formset = DocumentFormSet(self.request.POST, prefix='document')
+            # if not document_formset.is_valid():
+                # return JsonResponse({'success': False}, status=400)
             return JsonResponse({'success': True}, status=200)
-        return JsonResponse({'success': False}, status=400)
 
 
 @login_required(login_url='login')
