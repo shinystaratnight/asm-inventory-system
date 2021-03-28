ad315d67e802d4fd5e88b0413c91352c8d0c3153
Chris K <star-atnight@outlook.com>
2021-03-28 17:05:58 +0800

    add context_proccessor

---
+++ core/context_processors.py
@@ -0,0 +1,12 @@
+def seller(request):
+    return {
+        "seller_postal_code": '537―0021',
+        'seller_address': '大阪府大阪市東成区東中本2丁目4―15',
+        'seller_company': 'バッジオ株式会社',
+        'seller_ceo': '金 昇志',
+        'seller_tel': '06-6753-8078',
+        'seller_fax': '06-6753-8078',
+
+        'seller_payee_account': 'りそな銀行　船場支店（101）　普通　0530713　バッジオカブシキガイシャ',
+        'seller_reply_fax': '06-6753-8079'
+    }
