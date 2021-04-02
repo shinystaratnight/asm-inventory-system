a47c061e6e73c3cf32f13b699334857a0a110758
Chris K <star-atnight@outlook.com>
2021-04-02 22:01:37 +0800

    add japanese localization to bootstrap-datepicker

--- static/js/customs/picker_date.js
+++ static/js/customs/picker_date.js
@@ -17,8 +17,21 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // Single picker
+    $.fn.datepicker.dates['ja'] = {
+        days: [ "日曜日","月曜日","火曜日","水曜日","木曜日","金曜日","土曜日" ],
+        daysShort: [ "日","月","火","水","木","金","土" ],
+        daysMin: [ "日","月","火","水","木","金","土" ],
+        months: [ "1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月" ],
+        monthsShort: [ "1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月" ],
+        today: "今日",
+        clear: "削除",
+        format: "yyyy/mm/dd",
+        titleFormat: "yyyy/mm/dd",
+        weekStart: 0,
+    };
+
     $('.datepicker-milestone').datepicker({
-        'defaultDate': null
+        language: lang,
     });
     
 });
