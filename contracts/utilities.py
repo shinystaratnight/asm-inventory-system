22f3d0d13c2f4a55ad0f21e2c0a31d08cf1f6f02
Chris K <star-atnight@outlook.com>
2021-04-05 23:28:38 +0800

    fix up

--- contracts/utilities.py
+++ contracts/utilities.py
@@ -1,4 +1,14 @@
 import time
+from contracts.models import *
 
-def generate_contract_id():
-    return int(time.time() * 100)
+def generate_contract_id(prefix='01'):
+    year = time.strftime("%y", time.localtime())
+    month = time.strftime("%m", time.localtime())
+    day = time.strftime("%d", time.localtime())
+    contract_id = prefix + year + month + day
+    count = TraderSalesContract.objects.count()
+    count += TraderPurchasesContract.objects.count()
+    count += HallSalesContract.objects.count()
+    count += HallPurchasesContract.objects.count()
+    contract_id += str(count + 1).zfill(5)
+    return contract_id
