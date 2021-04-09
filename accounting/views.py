797c624c912b675662e71a636450b4fc71dd6ed7
Chris K <star-atnight@outlook.com>
2021-04-09 19:36:47 +0800

    fix up

--- accounting/views.py
+++ accounting/views.py
@@ -1,4 +1,4 @@
-import csv
+import unicodecsv as csv
 import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -10,13 +10,12 @@ from users.views import AdminLoginRequiredMixin
 from contracts.models import *
 from contracts.utilities import generate_random_number
 
-paginate_by = 5
-
+paginate_by = 3
 
 class SalesListView(AdminLoginRequiredMixin, TemplateView):
     template_name = 'accounting/sales.html'
     
-    def get_querylist(self):
+    def get_contract_list(self):
         trader_qs = TraderSalesContract.objects.all()
         hall_qs = HallSalesContract.objects.all()
         params = self.request.GET.copy()
@@ -36,13 +35,16 @@ class SalesListView(AdminLoginRequiredMixin, TemplateView):
                     trader_qs = trader_qs.filter(customer__name__icontains=v)
                     hall_qs = hall_qs.filter(customer__name__icontains=v)
         contract_list = list(trader_qs) + list(hall_qs)
-        page = self.request.POST.get('page', 1)
-        paginator = Paginator(contract_list, paginate_by)
+        return contract_list
+    
+    def get_paginator(self):
+        page = self.request.GET.get('page', 1)
+        paginator = Paginator(self.get_contract_list(), paginate_by)
         return paginator.page(page)
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['contracts'] = self.get_querylist()
+        context['page_obj'] = self.get_paginator()
         params = self.request.GET
         for k, v in params.items():
             if v:
@@ -56,15 +58,24 @@ class SalesListView(AdminLoginRequiredMixin, TemplateView):
         response = HttpResponse(content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename="accounting_sales_{}.csv"'.format(generate_random_number())
         writer = csv.writer(response, encoding='utf-8-sig')
-        writer.writerow(['1617130845', '2021/03/31', _('Income'), '課税売上10%', '93500', '（株）ライム'])
-        writer.writerow(['1617130845', '2021/03/31', '', '非課売上', '100', '（株）ライム'])
+        writer.writerow([
+            _('Contract ID'), _('Contract date'), _('Type'), _('Taxation'), _('Amount'), _('Customer'),
+        ])
+        contract_list = self.get_contract_list()
+        for contract in contract_list:
+            writer.writerow([
+                contract.contract_id, contract.created_at, _('Income'), '課税売上10%', contract.taxed_total, contract.customer.name
+            ])
+            writer.writerow([
+                contract.contract_id, contract.created_at, _('Income'), '非課売上', contract.insurance_fee, contract.customer.name
+            ])
         return response
 
 
 class PurchasesListView(AdminLoginRequiredMixin, TemplateView):
     template_name = 'accounting/purchases.html'
     
-    def get_querylist(self):
+    def get_contract_list(self):
         trader_qs = TraderPurchasesContract.objects.all()
         hall_qs = HallPurchasesContract.objects.all()
         params = self.request.GET.copy()
@@ -84,16 +95,16 @@ class PurchasesListView(AdminLoginRequiredMixin, TemplateView):
                     trader_qs = trader_qs.filter(customer__name__icontains=v)
                     hall_qs = hall_qs.filter(customer__name__icontains=v)
         contract_list = list(trader_qs) + list(hall_qs)
-        page = self.request.POST.get('page', 1)
-        paginator = Paginator(contract_list, paginate_by)
+        return contract_list
+    
+    def get_paginator(self):
+        page = self.request.GET.get('page', 1)
+        paginator = Paginator(self.get_contract_list(), paginate_by)
         return paginator.page(page)
-
-    def get_queryset(self):
-        return self.queryset
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['contracts'] = self.get_querylist()
+        context['page_obj'] = self.get_paginator()
         params = self.request.GET
         for k, v in params.items():
             if v:
@@ -107,6 +118,15 @@ class PurchasesListView(AdminLoginRequiredMixin, TemplateView):
         response = HttpResponse(content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename="accounting_purchases_{}.csv"'.format(generate_random_number())
         writer = csv.writer(response, encoding='utf-8-sig')
-        writer.writerow(['1617130845', '2021/03/31', _('Expense'), '課対仕入10%', '93500', '（株）ライム'])
-        writer.writerow(['1617130845', '2021/03/31', '', '非課仕入', '260', '（株）ライム'])
+        writer.writerow([
+            _('Contract ID'), _('Contract date'), _('Type'), _('Taxation'), _('Amount'), _('Customer'),
+        ])
+        contract_list = self.get_contract_list()
+        for contract in contract_list:
+            writer.writerow([
+                contract.contract_id, contract.created_at, _('Expense'), '課対仕入10%', contract.taxed_total, contract.customer.name
+            ])
+            writer.writerow([
+                contract.contract_id, contract.created_at, _('Income'), '非課仕入', contract.insurance_fee, contract.customer.name
+            ])
         return response
