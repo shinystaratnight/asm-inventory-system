f3b95859503c6f56bd618cd6c6ab2749dc66946d
Christopher Kim <star-atnight@outlook.com>
2021-05-06 13:59:37 +0800

    Change to settings.bk file

--- core/settings.bk.py
+++ core/settings.bk.py
@@ -94,6 +94,16 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'inventory',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
